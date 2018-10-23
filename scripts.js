25a513663292dbfcd84d1d54e26d3e9cd47615b6
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-23 20:11:30 -0300

    Incluir página de login

--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
-const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
-const linkFazerLogin = <a class="link" href="/login">Fazer login</a>
+function Link(props) {
+    return (
+        <a className="link" href={props.url}>
+            {props.children}
+        </a>
+    )
+}
 
-const formularioLogin = (
-    <form>
-        {[linkCriarUmaConta, linkFazerLogin]}
-    </form>
+function Botao(props) {
+    return (
+        <input className="botao" type="submit" value={props.children}/>
+    )
+}
+
+const paginaLogin = (
+    <main className="pagina-login">
+        <h1>Login</h1>
+        <p>Entre com seu email e senha.</p>
+        <Link url="/conta">Criar uma conta</Link>
+        <Botao>Enviar</Botao>
+    </main>
 )
 
 const divisaoProjeto = document.getElementById('projeto')
-
-ReactDOM.render(formularioLogin, divisaoProjeto)
+ReactDOM.render(paginaLogin, divisaoProjeto)
