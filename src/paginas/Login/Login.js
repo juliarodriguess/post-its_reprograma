cb81505c32e3a3662e831ebf9932daa7a9c205b2
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-24 21:41:29 -0300

    Criar arquivos separados para os componentes de botão e links

---
+++ src/paginas/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from '../../componentes/Link/Link';
+import Botao from '../../componentes/Botao/Botao';
+import './Login.css'
+
+function Login()  {
+    return (
+        <main className="login">
+            <h1>Login</h1>
+            <p>Entre com seu email e senha.</p>
+            <Link url="/conta">Criar uma conta</Link>
+            <Link url="/login">Fazer login</Link>
+            <Botao>Enviar</Botao>
+        </main>
+    )
+}
+
+export default Login
