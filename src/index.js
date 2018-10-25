34e6cdb433c48972177539006619cba5c84649bc
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-24 21:36:05 -0300

    Mudar estrutura de arquivos

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
-function Link(props) {
-    return (
-        <a className="link" href={props.url}>
-            {props.children}
-        </a>
-    )
-}
-
-function Botao(props) {
-    let classes = "botao"
-    if(props.desabilitado) {
-        classes += " botao--desabilitado"
-    } 
-    return (
-        <input className={classes} type="submit" value={props.children}/>
-    )
-}
-
-const paginaLogin = (
-    <main className="pagina-login">
-        <h1>Login</h1>
-        <p>Entre com seu email e senha.</p>
-        <Link url="/conta">Criar uma conta</Link>
-        <Link url="/login">Fazer login</Link>
-        <Botao>Enviar</Botao>
-    </main>
-)
+import Login from './paginas/Login/Login';
 
 const divisaoProjeto = document.getElementById('projeto')
-ReactDOM.render(paginaLogin, divisaoProjeto)
+
+ReactDOM.render(<Login />, divisaoProjeto)
