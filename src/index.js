7212aae99f280c0af3368b517fc71a66914fc080
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-25 21:03:45 -0300

    Incluir página de conta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Login from './paginas/Login/Login';
+import Conta from './paginas/Conta/Conta';
 
 const divisaoProjeto = document.getElementById('projeto')
 
-ReactDOM.render(<Login />, divisaoProjeto)
+ReactDOM.render(<Conta />, divisaoProjeto)
---
+++ src/paginas/Conta/Conta.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from '../../componentes/Link/Link';
+import Botao from '../../componentes/Botao/Botao';
+import Legenda from '../../componentes/Legenda/Legenda'
+import Campo from '../../componentes/Campo/Campo'
+import './Conta.css'
+
+function Conta()  {
+    return (
+        <main className="conta">
+            <h1>Conta</h1>
+            <p>Envie o formulário para criar uma conta!</p>
+            <Legenda htmlFor="nome">Nome:</Legenda>
+            <Campo id="nome" name="nome" type="text" placeholder="Nome"/> 
+            <Legenda htmlFor="telefone">Telefone:</Legenda>
+            <Campo id="telefone" name="telefone" type="tel" placeholder="Telefone"/>
+            <Legenda htmlFor="email">Email:</Legenda>
+            <Campo id="email" name="email" type="text" placeholder="Email"/> 
+            <Legenda htmlFor="senha">Senha:</Legenda>
+            <Campo id="senha" name="senha" type="password" placeholder="Senha"/>
+            <Botao>Enviar</Botao>
+            <Link url="/login">Fazer login</Link>
+        </main>
+    )
+}
+
+export default Conta
