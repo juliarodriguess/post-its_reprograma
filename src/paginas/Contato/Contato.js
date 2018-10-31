88b16e4a0cab2326e80d9feb66feea5413575a1e
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-30 22:24:49 -0300

    Criar novas páginas de contato e quem somos

---
+++ src/paginas/Contato/Contato.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Contato.css'
+
+function Contato() {
+    return (
+        <main className="contato">
+            <h1>Contato</h1>
+            <p>
+            Eat half my food and ask for more lick human with sandpaper tongue but if human is on laptop sit on the keyboard cat dog hate mouse eat string barf pillow no baths hate everything. I heard this rumor where the humans are our owners, pfft, what do they know?! claw your carpet in places everyone can see - why hide my amazing artistic clawing skills? so eat an easter feather as if it were a bird then burp victoriously, but tender, meowzer. Eat the fat cats food sit on human or paw at beetle and eat it before it gets away sleep everywhere, but not in my bed. Chase imaginary bugs when in doubt, wash stuff and things kick up litter sleep on dog bed, force dog to sleep on floor yet gnaw the corn cob chill on the couch table. Spend six hours per day washing, but still have a crusty butthole scratch leg; meow for can opener to feed me flex claws on the human's belly and purr like a lawnmower for touch water with paw then recoil in horror and then cats take over the world do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life, but purr. Asdflkjaertvlkjasntvkjn (sits on keyboard) sniff other cat's butt and hang jaw half open thereafter and annoy the old grumpy cat, start a fight and then retreat to wash when i lose i like big cats and i can not lie russian blue. Give me attention or face the wrath of my claws caticus cuteicus. 
+            </p>
+        </main>
+    )
+}
+
+export default Contato
