3498087e5e751ec5ea314fba4e4daf401f6950e2
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-11-07 22:27:53 -0200

    Incluir loading e espaço para post-its

--- src/paginas/Home/Home.js
+++ src/paginas/Home/Home.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import carregando from './loading.svg'
 import './Home.css'
 
-function Home(props) {
-    if(!props.usuario) {
-        return <Redirect to="/login"/>
-        
+class Home extends Component {
+    constructor (props) {
+        super(props)
+        this.state = { carregando: true }
     }
+    
+    
+    render() {
+        if(!this.props.usuario) {
+          return <Redirect to="/login"/>    
+        }
+
         return (
-            <main className='home' />
+            <main className='home'>
+                {this.state.carregando ? (
+                    <img className="home__loading" src={carregando} alt="Carregando" />
+                ):(
+                    <div>
+                        Aqui listarei os post its amanhã!
+                    </div>
+                )
+                }
+            </main>
         )
+    }
 }
 
 export default connect(
